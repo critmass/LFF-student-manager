5b4de2c173a8789842f35ab35d41466ddf4e4f7b
Jason <physics4life.jgcox@gmail.com>
2021-03-20 13:41:16 -0700

    clean up and removing debugger

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-toolbar = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+# toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
